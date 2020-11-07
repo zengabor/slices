84c55b0ce408ae8835d9961099fca228ca54d601
Gabor Lenard <zengabor@users.noreply.github.com>
2020-11-07 14:06:18 +0100

    Update slices_test.go

--- slices_test.go
+++ slices_test.go
@@ -1,17 +1,87 @@
 package slices
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func TestWithoutThis(t *testing.T) {
-	t.Fatal("test not implemented")
+	s := []string{"a", "b", "c", "d", "e"}
+	if fmt.Sprintf("%v", WithoutThis(s, "a")) != "[b c d e]" {
+		t.Error(WithoutThis(s, "a"))
+	}
+	if fmt.Sprintf("%v", WithoutThis(s, "c")) != "[a b d e]" {
+		t.Error(WithoutThis(s, "c"))
+	}
+	if fmt.Sprintf("%v", WithoutThis(s, "e")) != "[a b c d]" {
+		t.Error(WithoutThis(s, "e"))
+	}
+	if fmt.Sprintf("%v", WithoutThis(s, "A")) != "[a b c d e]" {
+		t.Error(WithoutThis(s, "A"))
+	}
+	if fmt.Sprintf("%v", WithoutThis(s, "x")) != "[a b c d e]" {
+		t.Error(WithoutThis(s, "x"))
+	}
+	if fmt.Sprintf("%v", WithoutThis([]string{}, "x")) != "[]" {
+		t.Error(WithoutThis([]string{}, "x"))
+	}
 }
 
 func TestIndex(t *testing.T) {
-	t.Fatal("test not implemented")
+	s := []string{"a", "b", "c", "d", "e"}
+	if Index(s, "a") != 0 {
+		t.Error(Index(s, "a"))
+	}
+	if Index(s, "c") != 2 {
+		t.Error(Index(s, "c"))
+	}
+	if Index(s, "e") != 4 {
+		t.Error(Index(s, "e"))
+	}
+	if Index(s, "A") != -1 {
+		t.Error(Index(s, "A"))
+	}
+	if Index(s, "x") != -1 {
+		t.Error(Index(s, "x"))
+	}
+	if Index(s, "") != -1 {
+		t.Error(Index(s, ""))
+	}
+}
+
+func TestContains(t *testing.T) {
+	s := []string{"a", "b", "c", "d", "e"}
+	if !Contains(s, "a") {
+		t.Error(Contains(s, "a"))
+	}
+	if !Contains(s, "c") {
+		t.Error(Contains(s, "c"))
+	}
+	if !Contains(s, "e") {
+		t.Error(Contains(s, "e"))
+	}
+	if Contains(s, "A") {
+		t.Error(Contains(s, "A"))
+	}
+	if Contains(s, "x") {
+		t.Error(Contains(s, "x"))
+	}
+	if Contains(s, "") {
+		t.Error(Contains(s, ""))
+	}
 }
 
 func TestAppendIfNotFound(t *testing.T) {
-	t.Fatal("test not implemented")
+	s := []string{"a", "b", "c", "d", "e"}
+	if fmt.Sprintf("%v", AppendIfNotFound(s, "a")) != "[a b c d e]" {
+		t.Error(AppendIfNotFound(s, "a"))
+	}
+	if fmt.Sprintf("%v", AppendIfNotFound(s, "e")) != "[a b c d e]" {
+		t.Error(AppendIfNotFound(s, "e"))
+	}
+	if fmt.Sprintf("%v", AppendIfNotFound(s, "x")) != "[a b c d e x]" {
+		t.Error(AppendIfNotFound(s, "x"))
+	}
 }
 
 func TestMergeStringSlices(t *testing.T) {
@@ -24,8 +94,11 @@ func TestMergeStringSlices(t *testing.T) {
 		{[]string{"s2", "s1"}, []string{}, "", []string{"s1", "s2"}},
 		{[]string{"s2", "s1"}, []string{"s1"}, "", []string{"s1", "s2"}},
 		{[]string{"s2", "s1"}, []string{"s1", "s2"}, "", []string{"s1", "s2"}},
+		{[]string{"s2", "", "s1"}, []string{"s1", "s2"}, "", []string{"s1", "s2"}},
+		{[]string{"s2", "s1"}, []string{"s1", "s2", ""}, "", []string{"s1", "s2"}},
 		{[]string{"s2"}, []string{"s1", "s2"}, "", []string{"s1", "s2"}},
 		{[]string{"s2"}, []string{"s1"}, "", []string{"s1", "s2"}},
+		{[]string{"s1"}, []string{"s2"}, "s1", []string{"s2"}},
 		{[]string{"s2"}, []string{"s1"}, "s1", []string{"s2"}},
 		{[]string{"s2"}, []string{"s1"}, "s3", []string{"s1", "s2"}},
 		{[]string{}, []string{}, "", []string{}},
