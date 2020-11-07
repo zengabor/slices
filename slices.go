0c29c199e98f4780bac8225f0988b67e05219522
Gabor Lenard <zengabor@users.noreply.github.com>
2020-11-07 14:06:12 +0100

    Update slices.go

--- slices.go
+++ slices.go
@@ -2,7 +2,12 @@ package slices
 
 import "sort"
 
+// WithoutThis returns a new slice without the specified item.
+// If the specified item is not found, all items get included in the new slice.
 func WithoutThis(slice []string, withoutThis string) []string {
+	if len(slice) < 1 {
+		return slice
+	}
 	cc := make([]string, 0, len(slice)-1)
 	for _, s := range slice {
 		if s != withoutThis {
@@ -12,6 +17,7 @@ func WithoutThis(slice []string, withoutThis string) []string {
 	return cc
 }
 
+// Index returns the index of the specified item, or -1 if it's not found.
 func Index(slice []string, needle string) (index int) {
 	var s string
 	for index, s = range slice {
@@ -22,33 +28,37 @@ func Index(slice []string, needle string) (index int) {
 	return -1
 }
 
+// Contains reports whether the specified item is found in the slice.
 func Contains(slice []string, needle string) bool {
 	return Index(slice, needle) != -1
 }
 
-func AppendIfNotFound(slice []string, s string) (newSlice []string) {
-	if Index(slice, s) != -1 {
+// AppendIfNotFound returns a slice with the item appended if it's not yet in the slice.
+func AppendIfNotFound(slice []string, s string) []string {
+	if !Contains(slice, s) {
 		return append(slice, s)
 	}
 	return slice
 }
 
-func Merge(s1, s2 []string, skip string) []string {
+// Merge merges two slices into a single slice, skipping withoutThis.
+// Empty values are also skipped.
+func Merge(s1, s2 []string, withoutThis string) []string {
 	unique := make(map[string]struct{})
 	for _, v := range s1 {
-		if len(v) < 1 {
+		if len(v) == 0 {
 			continue
 		}
-		if v == skip {
+		if v == withoutThis {
 			continue
 		}
 		unique[v] = struct{}{}
 	}
 	for _, v := range s2 {
-		if len(v) < 1 {
+		if len(v) == 0 {
 			continue
 		}
-		if v == skip {
+		if v == withoutThis {
 			continue
 		}
 		unique[v] = struct{}{}
